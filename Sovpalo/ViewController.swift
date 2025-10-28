c9c1d16857c496ecd5649d2e372187cae68110ac
vovaggri <vladimirgri0405@gmail.com>
2025-10-28 17:58:43 +0300

    files

---
+++ Sovpalo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sovpalo
+//
+//  Created by Vladimir Grigoryev on 28.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
